6b57366593f91648c1ad02e3849f582a1f4cd5da
lemon24 <damian.adrian24@gmail.com>
2025-08-13 00:56:27 +0300

    Add article code as-is.

--- src/asyncio_thread_runner.py
+++ src/asyncio_thread_runner.py
@@ -3,4 +3,90 @@ Run async Python code from sync code.
 
 """
 
+import asyncio
+import contextlib
+import sys
+import threading
+
 __version__ = "0.1.dev0"
+
+
+class ThreadRunner:
+
+    def __init__(self, *args, **kwargs):
+        self._runner = asyncio.Runner(*args, **kwargs)
+        self._thread = None
+        self._stack = contextlib.ExitStack()
+
+    def __enter__(self):
+        self._lazy_init()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        try:
+            self._stack.close()
+        finally:
+            loop = self.get_loop()
+            loop.call_soon_threadsafe(loop.stop)
+            self._thread.join()
+
+    def get_loop(self):
+        self._lazy_init()
+        return self._runner.get_loop()
+
+    def run(self, coro):
+        loop = self.get_loop()
+        return asyncio.run_coroutine_threadsafe(coro, loop).result()
+
+    def _lazy_init(self):
+        if self._thread:
+            return
+
+        loop_created = threading.Event()
+
+        def run_forever():
+            with self._runner as runner:
+                loop = runner.get_loop()
+                asyncio.set_event_loop(loop)
+                loop_created.set()
+                loop.run_forever()
+
+        self._thread = threading.Thread(
+            target=run_forever, name='LoopThread', daemon=True
+        )
+        self._thread.start()
+        loop_created.wait()
+
+    @contextlib.contextmanager
+    def wrap_context(self, cm=None, factory=None):
+        if cm is None:
+            cm = self.run(call_async(factory))
+        aenter = type(cm).__aenter__
+        aexit = type(cm).__aexit__
+        value = self.run(aenter(cm))
+        try:
+            yield value
+        except:
+            if not self.run(aexit(cm, *sys.exc_info())):
+                raise
+        else:
+            self.run(aexit(cm, None, None, None))
+
+    def enter_context(self, cm=None, factory=None):
+        cm = self.wrap_context(cm=cm, factory=factory)
+        return self._stack.enter_context(cm)
+
+    def wrap_iter(self, it):
+        it = aiter(it)
+        while True:
+            try:
+                yield runner.run(anext(it))
+            except StopAsyncIteration:
+                break
+
+
+async def call_async(callable):
+    return callable()
